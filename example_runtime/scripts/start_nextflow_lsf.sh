98a7d89224170c8e98e41d01586b91650b9879b8
Guillaume G Noell <gn5@sanger.ac.uk>
2021-10-13 17:02:06 +0100

    auto-git

--- example_runtime/scripts/start_nextflow_lsf.sh
+++ example_runtime/scripts/start_nextflow_lsf.sh
@@ -8,5 +8,5 @@ conda activate nextflow
 # run nextflow main.nf with inputs and lsf config:
 export NXF_OPTS="-Xms5G -Xmx5G"
 nextflow run ./pipelines/main.nf \
-      -c ./nextflow_ci/nextflow.config -c inputs.nf -profile lsf \
+      -c ./nextflow.config -c inputs.nf -profile lsf \
       --nf_ci_loc $PWD -resume
