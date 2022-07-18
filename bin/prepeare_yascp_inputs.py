36e385942c38846f978648056b261f0ae293bfb3
Matiss Ozols <mo11@sanger.ac.uk>
2022-07-18 10:43:48 +0100

    fixed the downstream effects of dublicated viability retrieval

--- bin/prepeare_yascp_inputs.py
+++ bin/prepeare_yascp_inputs.py
@@ -250,7 +250,7 @@ def main():
             Data['n_pooled']=Reviewed_metadata['n_pooled']
             for idx1 in set(Data.index):
                 print(idx1)
-                dons = ','.join(list(Reviewed_metadata_donors.loc[idx1]['donor'].values))
+                dons = ','.join(set(list(Reviewed_metadata_donors.loc[idx1]['donor'].values)))
                 Data.loc[idx1,'donor_vcf_ids']='\''+dons+'\''
         except:
             Data['n_pooled']=1
