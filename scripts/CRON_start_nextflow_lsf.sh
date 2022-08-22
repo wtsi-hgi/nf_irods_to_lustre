2d35099f6c0d49eca6919caaa633eeb5ff5fe104
Matiss Ozols <mo11@sanger.ac.uk>
2022-08-22 11:56:36 +0100

    try again

--- scripts/CRON_start_nextflow_lsf.sh
+++ scripts/CRON_start_nextflow_lsf.sh
@@ -17,8 +17,8 @@ rm -f nextflow.nohup.PID.txt
 
 # start Nextflow in background:
 export NXF_OPTS="-Xms5G -Xmx5G"
-nextflow run ./nf_irods_to_lustre/pipelines/main.nf \
-      -c ./nf_irods_to_lustre/nextflow.config --resume -c ./nf_irods_to_lustre/scripts/inputs.nf -profile lsf \
+nextflow run $PWD/nf_irods_to_lustre/pipelines/main.nf \
+      -c $PWD/nf_irods_to_lustre/nextflow.config --resume -c $PWD/nf_irods_to_lustre/scripts/inputs.nf -profile lsf \
       -with-dag flowchart.png -with-report report.html --resume --nf_ci_loc $PWD > nextflow.nohup.log
 
 echo DONE
