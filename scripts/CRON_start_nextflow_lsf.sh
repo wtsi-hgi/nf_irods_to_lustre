fed8333e0316a7c4ea045f274a6ba159f101a86b
Matiss Ozols <mo11@sanger.ac.uk>
2022-08-22 11:22:08 +0100

    test cron

--- scripts/CRON_start_nextflow_lsf.sh
+++ scripts/CRON_start_nextflow_lsf.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+SHELL=/bin/bash
+BASH_ENV=/lustre/scratch123/hgi/projects/ukbb_scrna/pipelines/Pilot_UKB/fetch/SETUP_fech_input_prep/.bashrc_conda
+# activate Nextflow conda env
+eval "$(cat /lustre/scratch123/hgi/projects/ukbb_scrna/pipelines/Pilot_UKB/fetch/SETUP_fech_input_prep/.bashrc_conda | tail -n +10)"
+conda init bash
+eval "$(conda shell.bash hook)"
 
 # activate Nextflow conda env
-/software/hgi/installs/anaconda3/bin/conda init bash
+conda init bash
 eval "$(conda shell.bash hook)"
-/software/hgi/installs/anaconda3/bin/conda activate nextflow
+conda activate nextflow
 
 # clean up previous run files
 rm -f *.log
@@ -11,7 +17,7 @@ rm -f nextflow.nohup.PID.txt
 
 # start Nextflow in background:
 export NXF_OPTS="-Xms5G -Xmx5G"
-/software/hgi/installs/anaconda3/envs/mo11/bin/nextflow run ./nf_irods_to_lustre/pipelines/main.nf \
+nextflow run ./nf_irods_to_lustre/pipelines/main.nf \
       -c ./nf_irods_to_lustre/nextflow.config --resume -c ./nf_irods_to_lustre/scripts/inputs.nf -profile lsf \
       -with-dag flowchart.png -with-report report.html --resume --nf_ci_loc $PWD > nextflow.nohup.log
 
