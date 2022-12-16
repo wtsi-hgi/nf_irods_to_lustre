84b57e695e44b5ce7c756b69a8df343ac470018f
Matiss Ozols <mo11@sanger.ac.uk>
2022-12-16 14:02:03 +0000

    fx

---
+++ scripts/bsub.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sample='Fetch'
+bsub -R'select[mem>4000] rusage[mem=4000]' -J $sample -n 1 -M 4000 -o $sample.o -e $sample.e -q basement bash nf_irods_to_lustre/scripts/nohup_start_nextflow_lsf.sh
